78a2b8208bf24c89a86704c7795f6f4bed448bbb
Elllion <32142429+Elllion@users.noreply.github.com>
2018-03-24 11:39:30 -0400

    Comments and Tweaking
    Empty list fix
    File format fix
    Commented everything
    Client/Server can be built + run with gradle

---
+++ Client/src/main/java/Main.java
@@ -0,0 +1,227 @@
+package main.java;
+
+import java.io.*;
+import java.net.Socket;
+
+import javafx.scene.*;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.*;
+
+
+
+public class Main extends Application{
+    private BorderPane layout;
+    static ListView<String> clientFiles = new ListView<>();
+    static ListView<String> serverFiles = new ListView<>();
+    private static File localFolder = new File("data");
+
+    private static String serverIP = "127.0.0.1";
+    private static int serverPort = 8888;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Assignment 2");
+
+        GridPane editArea = new GridPane();
+        editArea.setPadding(new Insets(10, 10, 10, 10));
+        editArea.setVgap(10);
+        editArea.setHgap(10);
+
+        //Download button
+        Button dlButton = new Button("Download Selected");
+        editArea.add(dlButton, 1, 0);
+        dlButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                //Don't do anything if nothing is selected
+                if(serverFiles.getSelectionModel().getSelectedItem() == null)
+                    return;
+
+
+                try {
+                    //Connect to server
+                    Socket socket = new Socket(serverIP, serverPort);
+                    PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+                    //Send request
+                    out.print("DOWNLOAD " + serverFiles.getSelectionModel().getSelectedItem());
+                    out.flush();
+                    socket.shutdownOutput();//Shutdown once output sent
+
+
+                    InputStream in = socket.getInputStream();
+                    InputStreamReader reader = new InputStreamReader(in);
+                    BufferedReader bin = new BufferedReader(reader);
+                    String line = null;
+
+                    //Open the file locally
+                    File saveTo = new File(localFolder.getName() + "/" + serverFiles.getSelectionModel().getSelectedItem());
+                    PrintWriter saver= new PrintWriter(saveTo);
+                    StringBuilder fileText = new StringBuilder();
+
+                    //Receive data from the server
+                    while ((line = bin.readLine()) != null) {
+                        fileText.append(line);
+                        //\n was not starting a new line, so line.separator is used
+                        fileText.append(System.getProperty("line.separator"));
+
+                    }
+
+                    //Save data to file
+                    saver.write(fileText.toString());
+
+                    //Close everything
+                    saver.close();
+                    in.close();
+                    reader.close();
+                    bin.close();
+                    out.close();
+                    socket.close();
+
+                    //Update local file list
+                    updateFiles();
+                }catch (IOException e){
+                    System.out.println("IOException");
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        //Upload button
+        Button ulButton = new Button("Upload Selected");
+        editArea.add(ulButton, 0, 0);
+        ulButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                //Don't do anything if nothing is selected
+                if(clientFiles.getSelectionModel().getSelectedItem() == null)
+                    return;
+
+                try {
+                    //Connect to server
+                    Socket socket = new Socket(serverIP, serverPort);
+                    PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+                    //Send request
+                    String outPacket = "UPLOAD " + clientFiles.getSelectionModel().getSelectedItem() + "\n";
+
+                    //Open the file locally
+                    BufferedReader br = new BufferedReader(new FileReader(new File(localFolder.getName() + "/" + clientFiles.getSelectionModel().getSelectedItem())));
+
+                    //Send the file line by line to the server
+                    String line = "";
+                    out.print(outPacket);
+                    while((line = br.readLine()) != null){
+                      out.println(line);
+                    }
+
+                    //Close everything
+                    br.close();
+                    out.flush();
+                    socket.shutdownOutput();//Shutdown once output sent
+                    out.close();
+                    socket.close();
+
+                    //Update server file list
+                    getServerFiles();
+                }catch (IOException e){
+                    System.out.println("IOException");
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        //Lists for local/server files
+        editArea.add(clientFiles,0,1);
+        editArea.add(serverFiles,1,1);
+        // initialize the border pane
+        layout = new BorderPane();
+        layout.setTop(editArea);
+
+        Scene scene = new Scene(layout, 500, 450);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        updateFiles();
+
+    }
+
+    public static void updateFiles(){
+        clientFiles.getItems().clear();
+        File[] fileList = localFolder.listFiles();
+
+        //Don't add anything if the local folder is empty
+        if(fileList.length == 0)
+            return;
+        //Read every file in the folder
+        for(File f : fileList){
+            if(f.isFile())
+                clientFiles.getItems().add(f.getName());
+        }
+    }
+
+    public static void getServerFiles(){
+        try {
+            serverFiles.getItems().clear();
+
+            //Connect to server
+            Socket socket = new Socket(serverIP, serverPort);
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+            //Send request
+            out.print("DIR");
+            out.flush();
+            socket.shutdownOutput();//Shutdown once output sent
+
+            InputStream in = socket.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            BufferedReader bin = new BufferedReader(reader);
+            String line = null;
+
+            //Receive data
+            while (line == null) {
+                line = bin.readLine();
+            }
+
+            //Separate data by slashes since they can't be used in filenames
+            String[] data = line.split("/");
+
+            //Add each item to the server file list
+            for(String s : data) {
+                serverFiles.getItems().add(s);
+            }
+
+            //Close everything
+            in.close();
+            reader.close();
+            bin.close();
+            out.close();
+            socket.close();
+        }catch (IOException e){
+            System.out.println("IOException");
+            e.printStackTrace();
+        }
+    }
+    public static void main(String [] args) {
+
+        if(args.length < 1){
+            System.out.println("No arguments passed. Required arguments: \n-Path to shared folder\n\n" +
+                    "Optional arguments:\n-Server IP\n-Server Port");
+            System.exit(0);
+        }
+        if(args.length > 1){
+            serverIP = args[1];
+        }
+        if(args.length > 2){
+            serverPort = Integer.parseInt(args[2]);
+        }
+        getServerFiles();
+        localFolder = new File(args[0]);
+        launch(args);
+    }
+}
---
+++ Server/src/main/java/Main.java
@@ -0,0 +1,161 @@
+package  main.java;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.UnsupportedAddressTypeException;
+
+class ClientConnectionHandler extends Thread{
+    Socket clientSocket;
+  ClientConnectionHandler(Socket clientSocket){
+        this.clientSocket = clientSocket;
+  }
+
+  public void run(){
+      try {
+          InputStream cin = clientSocket.getInputStream();
+          InputStreamReader creader = new InputStreamReader(cin);
+          BufferedReader cbin = new BufferedReader(creader);
+          String cline = null;
+
+          System.out.println("Connected to server");
+
+          //Wait for client's command
+          while ((cline = cbin.readLine()) == null) {
+              System.out.println("Waiting for message...");
+          }
+          clientSocket.shutdownInput();//Shutdown input once sent
+          System.out.println(cline);
+
+          //Separate command by spaces
+          String[] command = cline.split(" ");
+
+
+          if(command.length > 0) {
+              //First element of command array is the command type
+              //DIR - List all files in the server's folder
+              if (command[0].contains("DIR")){
+                  File sharedFiles[] = new File("files").listFiles();
+                  String data = "";
+
+                  //Separate file names by '/'
+                  for(File f: sharedFiles){
+                      if(f.isFile())
+                          data+= f.getName() + "/";
+                  }
+
+                  //Send the line to the client
+                  //Send a "blank" string if the server has no files
+                  PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+                  if(data.length() < 1)
+                      out.print("/");
+                  else
+                      out.print(data);
+
+                  //Close and disconnect
+                  out.flush();
+                  System.out.println("Message sent");
+                  clientSocket.close();
+
+              }
+              //DOWNLOAD - Client wants the specified file
+              else if(command[0].contains("DOWNLOAD")){
+                  String data = command[1];
+
+                  String filePath = "files/" + command[1];
+                  File sendTo = new File(filePath);
+
+                  //Because the command line is separated by spaces,
+                  //The filename can be cut off.
+                  //To fix this, check if the argument is a valid file
+                  //and add extra command tokens to it if it is not.
+                  int i = 2;
+                  while(!sendTo.exists() && i < command.length){
+                      filePath += " " + command[i];
+                      i++;
+                      sendTo = new File(filePath);
+                      System.out.println(filePath);
+                  }
+
+                  //Open the file
+                  BufferedReader br = new BufferedReader(new FileReader(sendTo));
+                  PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+                  String line = "";
+
+                  while((line = br.readLine()) != null){
+                      out.println(data);
+                  }
+
+
+                  //Close and disconnect
+                  out.flush();
+                  System.out.println("Message sent");
+                  clientSocket.close();
+              }
+              //UPLOAD - Client is sending a file
+              else if(command[0].contains("UPLOAD")){
+                  String data = command[1];
+
+                  //Because the command line is separated by spaces,
+                  //The filename can be cut off.
+                  //To fix this, check if the argument is a valid file
+                  //and add extra command tokens to it if it is not.
+                  int i = 2;
+                  while(!data.contains(".") && i < command.length){
+                      data += " " + command[i];
+                      i++;
+                  }
+
+                  //Open the file
+                  File saveTo = new File("files/" + data);
+                  PrintWriter saver= new PrintWriter(saveTo);
+
+                  //Receive data from the user
+                  StringBuilder fileText = new StringBuilder();
+                  while((cline = cbin.readLine()) != null){
+                      System.out.println("Line: " + cline);
+                     fileText.append(cline);
+                     fileText.append(System.getProperty("line.separator"));
+                  }
+
+                  //Write to file
+                  saver.write(fileText.toString());
+
+                  //Close and disconnect
+                  saver.close();
+                  clientSocket.close();
+              }
+          }
+      }
+      catch(Exception e){
+          e.printStackTrace();
+      }
+  }
+}
+
+public class Main {
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+
+    public static void main(String [] args) {
+        try {
+            serverSocket = new ServerSocket(8888);
+
+            System.out.println("Ready");
+
+            //Constantly accept client connections
+            while(true){
+
+                clientSocket = serverSocket.accept();
+                if(clientSocket != null){
+                    //Open a thread if someone actually connects
+                    ClientConnectionHandler conn = new ClientConnectionHandler(clientSocket);
+                    conn.run();
+                }
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+}
